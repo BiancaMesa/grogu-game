3952c1e1e058787c064a76af6f4e256e712bb62f
BiancaMesa <bianca.koraa@gmail.com>
2024-03-13 11:00:45 +0100

    basic structure finished

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,52 @@
 import '../scss/App.scss'; 
 
 function App() {
-  return <p>Hola</p>
+  return (
+    <>
+      <header>
+        <h1 className="title">¡Cuidado con Grogu!</h1>
+      </header>
+      <main className="page">
+        <h3 className="subtitle">Introduce tu nombre para jugar</h3>
+        <input className="inputName" type="text" name="" id="" placeholder="Escribe tu nombre"/>
+
+        <section className="board">
+          <div className="cell"><div class="grogu">👣</div></div>
+          <div className="cell"></div>
+          <div className="cell"></div>
+          <div className="cell"></div>
+          <div className="cell"></div>
+          <div className="cell"></div>
+          <div className="cell"></div>
+        </section>
+
+        <section>
+          <button className="dice">Lanzar Dado</button>
+          <div className="game-status">En curso</div>
+        </section>
+
+        <section className="goods-container">
+          <div className="goods-item">🍪</div>
+          <div className="goods-item">🍪</div>
+          <div className="goods-item">🍪</div>
+        </section>
+        <section className="goods-container">
+          <div className="goods-item">🥚</div>
+          <div className="goods-item">🥚</div>
+          <div className="goods-item">🥚</div>
+        </section>
+        <section className="goods-container">
+          <div className="goods-item">🐸</div>
+          <div className="goods-item">🐸</div>
+          <div className="goods-item">🐸</div>
+        </section>
+        <section>
+          <button className="restart-button">Reiniciar Juego</button>
+        </section>
+      </main>
+
+    </>
+  )
 }
 
 export default App;
